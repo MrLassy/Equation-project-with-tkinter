a6cda4e9fa3a9497b7c05f532518397be6d1e5d5
Lassdev <lancinedoumbouya501@gmail.com>
2026-07-30 15:43:08 +0000

    MVP avec POO

--- equation.py
+++ equation.py
@@ -1,118 +1,137 @@
 import tkinter as tk 
+from tkinter import TclError
 from sympy import symbols, Eq, solve, pretty, sympify, SympifyError, Mul
 import random
 
-app = tk.Tk()
-app.title("Math - Equation")
-app.geometry("800x400")
+
+class JeuEquation():
+
+    def __init__(self):
+        self.app = tk.Tk()
+        self.equation = None
+        self.temps = 31
+        self.timer_id = None
+
+        # Déclaration des widgets 
+        self.label_equation = tk.Label(self.app, font=("Consolas", 12))
+        self.label_solution = tk.Label(self.app)
+        self.timer_label = tk.Label(self.app, font=("Arial", 12))
+        self.buttton_generate = tk.Button(self.app, text="Nouvelle partie", command=self.genere_equation, font=("Arial", 12))
+        self.buttton_reponse = tk.Button(self.app, text="Confirmer la réponse", command=self.solve_equation, font=("Arial", 12))
+        self.selected_option = tk.StringVar(value="Facile")
+        self.entry = tk.Entry(self.app, width=25, font=("Arial", 12))
 
 
-equation = None
-temps = 31
-timer_id = None
-# Option gestion
-selected_option = tk.StringVar(value="Facile")
+    def genere_equation(self) :
+        option = self.selected_option.get()        
+        x = symbols('x')
 
-# Déclaration des radiobutton
-rb1 = tk.Radiobutton(app, text="Facile", variable=selected_option, value="Facile")
-rb2 = tk.Radiobutton(app, text="Moyen", variable=selected_option, value="Moyen")
-rb3 = tk.Radiobutton(app, text="Difficile", variable=selected_option, value="Difficile")
+        if option == "Facile":
+            a = random.randint(1, 9)
+            b = random.randint(0, 10)
+            c = random.randint(-10, 10)
+            self.equation = Eq(a*x + b, c)
+            self.temps = 31
 
-#Widget 
-rb1.pack(anchor="w", padx=20, pady=5)
-rb2.pack(anchor="w", padx=20, pady=5)
-rb3.pack(anchor="w", padx=20, pady=5)
+        elif option == "Moyen":
 
+            a = random.randint(2, 9)
+            b = random.randint(1 , 5)
+            d = random.randint(-1, 10)
+            c =random.randint(-1, 10)
+            self.equation = Eq(Mul(a ,( x * b + d ), evaluate=False), c)
+            self.temps = 61
 
-def genere_equation():
-    global equation, temps, timer_id
-    
-    
-    option = selected_option.get()
-    # Equation 
-    x = symbols('x')
-
-    if option == "Facile":
-        a = random.randint(1, 9)
-        b = random.randint(0, 10)
-        c = random.randint(-10, 10)
-        equation = Eq(a*x + b, c)
-    elif option == "Moyen":
-
-        a = random.randint(2, 9)
-        b = random.randint(1 , 5)
-        d = random.randint(-1, 10)
-        c =random.randint(-1, 10)
-        equation = Eq(Mul(a ,( x * b + d ), evaluate=False), c)
-    else :
-        a = random.randint(1, 9)
-        b = random.randint(1, 9)
-        c = random.randint(-10, 10)        
-        equation = Eq( a*x**2 + b * x + c, 0)
-            
-    format = pretty(equation, use_unicode=True)
-    label_equation.config(text=format)
-
-    entry.delete(0 , tk.END)
-    label_solution.config(text="")
-    button2.config(state="normal")
-    if timer_id:
-        app.after_cancel(timer_id)
-    temps = 31
-    timer()
-
-def solve_equation():
-    
-    solution = list(solve(equation))
-    user_solution = entry.get()
-    try :   
-        if sympify(user_solution) in solution :
-            label_solution.config(text="Réponse correct", font=("Arial", 12), fg="green")
-            button2.config(state="disabled")
         else :
-            label_solution.config(text=f"It is a bad answer\nGood answer: {solution}", font=("Arial", 12), fg="red") 
-            button2.config(state="disabled")
-    except (SympifyError, SyntaxError, TypeError) :
-        if not user_solution.strip():
-            label_solution.config(text="Champ vide", font=("Arial", 12), fg="red")     
-        else :
-            label_solution.config(text="Entrée incorrect", font=("Arial", 12), fg="red")                       
+            a = random.randint(1, 9)
+            b = random.randint(1, 9)
+            c = random.randint(-10, 10)
+            self.equation = Eq( a*x**2 + b * x + c, 0)
+            self.temps = 91
+
+        format = pretty(self.equation, use_unicode=True)
+
+        self.label_equation.config(text=format)
+
+        self.entry.delete(0 , tk.END)
+        self.label_solution.config(text="")
+        self.buttton_reponse.config(state="normal")
+
+        if self.timer_id:
+            self.app.after_cancel(self.timer_id)
+        self.timer()
 
 
+    def solve_equation(self):
+        solution = list(solve(self.equation))
+        user_solution = self.entry.get()
 
-def timer():
-    global temps, timer_id
+        try :
 
-    if temps > 0 :
-        temps = temps - 1
-        timer_label.config(text=f"Temps restant: {temps}", fg="black")
-        timer_id = timer_label.after(1000, timer)
-    else :
-        timer_label.config(text="Temps écoulé !!", fg="red")
-        button2.config(state="disabled")
+            if sympify(user_solution) in solution :
+                self.label_solution.config(text="Réponse correct", font=("Arial", 12), fg="green")
 
+            else :
+                self.label_solution.config(text=f"It is a bad answer\nGood answer: {solution}", font=("Arial", 12), fg="red")
 
-label_equation = tk.Label(app,font=("Consolas", 12))
-label_equation.pack(pady=5)
+            self.buttton_reponse.config(state="disabled")
+        except (SympifyError, SyntaxError, TypeError) :
 
-entry = tk.Entry(app, width=25, font=("Arial", 12))
-entry.pack(pady=20)
-
-
-label_solution= tk.Label(app)
-label_solution.pack(pady=10)
+            if not user_solution.strip():
+                self.label_solution.config(text="Champ vide", font=("Arial", 12), fg="red")
+            else :
+                self.label_solution.config(text="Entrée incorrect", font=("Arial", 12), fg="red") 
 
 
-button1 = tk.Button(app, text="Commencer une nouvelle partie" , command=genere_equation)
-button1.pack(pady=0)
+    def timer(self):
+        if not self.timer_label.winfo_exists() :
+            return
+
+        try :
+            if self.temps > 0 :
+                self.temps -= 1
+                self.timer_label.config(text=f"Temps restant: {self.temps} s", fg="black")
+                self.timer_id = self.timer_label.after(1000, self.timer)
+            else :
+
+                self.timer_label.config(text="Temps écoulé !!", fg="red")
+                self.buttton_reponse.config(state="disabled") 
+        except TclError :
+            pass        
+
+    def on_closing(self):
+    
+        if self.timer_id:
+            self.app.after_cancel(self.timer_id)
+        self.app.quit()    
+        self.app.destroy()
+
+    def start(self):
+        self.app.geometry("800x400")
+        self.app.title("Math - Equation")
+
+        # Widget des button-radio 
+        rb1 = tk.Radiobutton(self.app, text="Facile", variable=self.selected_option, value="Facile")
+        rb2 = tk.Radiobutton(self.app, text="Moyen", variable=self.selected_option, value="Moyen")
+        rb3 = tk.Radiobutton(self.app, text="Difficile", variable=self.selected_option, value="Difficile")
+
+        # Mis en page des Widgets 
+        rb1.pack(anchor="w", padx=20, pady=5)
+        rb2.pack(anchor="w", padx=20, pady=5)        
+        rb3.pack(anchor="w", padx=20, pady=5)
+
+        self.label_equation.pack(pady=15)
+        self.entry.pack(pady=10)
+        self.label_solution.pack(pady=15)
+        self.buttton_generate.pack(pady=5)
+        self.buttton_reponse.pack(pady=5)
+
+        self.timer_label.pack(anchor="w", pady=15)
+
+        self.genere_equation()
+        self.app.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.app.mainloop()
 
 
-button2 = tk.Button(app, text="Confirmer la réponse", command=solve_equation, state="normal")
-button2.pack(pady=20)
-
-
-timer_label = tk.Label(font=("Arial", 12))
-timer_label.pack(anchor="w")
-
-genere_equation()
-app.mainloop()
+app = JeuEquation()
+app.start()
